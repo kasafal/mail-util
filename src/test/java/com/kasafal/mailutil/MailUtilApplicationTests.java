ba0fa5fb627f06c2657773a071e2072b3fdf16b9
Ajay <ajaydas.6080@gmail.com>
2020-03-08 00:37:32 +0530

    Initial commit

---
+++ src/test/java/com/kasafal/mailutil/MailUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasafal.mailutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
